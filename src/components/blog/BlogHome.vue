7b58c96b85bb1b04421fb18426580876008e0091
Sayedul Sayem <sayedulsayem@gmail.com>
2024-03-07 22:24:56 +0600

    after class

---
+++ src/components/blog/BlogHome.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { data, fn } from "../../data.js";
+import BlogCard from "../parts/BlogCard.vue";
+</script>
+<template>
+  <div class="grid grid-cols-3 gap-20 items-center justify-between p-24">
+    <BlogCard v-for="post in data.posts" :key="post.id" :post="post" />
+  </div>
+</template>
